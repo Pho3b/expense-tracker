437610f5e2ba1ff05e0e52cdd9384d3076dbca66
arinaldi <andrea.rinaldi2204@gmail.com>
2023-08-26 13:38:49 +0200

    refactor: minor code cleanup

--- app/src/main/java/com/example/expensetracker/fragment/DatePickerFragment.java
+++ app/src/main/java/com/example/expensetracker/fragment/DatePickerFragment.java
@@ -6,32 +6,25 @@ import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
 public class DatePickerFragment extends DialogFragment {
+    @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Get the current date
         Calendar calendar = Calendar.getInstance();
-        int year = calendar.get(Calendar.YEAR);
-        int month = calendar.get(Calendar.MONTH);
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-        // Create a DatePickerDialog and set the listener
-        DatePickerDialog datePickerDialog = new DatePickerDialog(
+        return new DatePickerDialog(
                 requireContext(),
-                (view, selectedYear, selectedMonth, selectedDay) -> {
-                    Toast.makeText(
-                            view.getContext(),
-                            (selectedYear + ":" + selectedMonth + ":" + selectedDay),
-                            Toast.LENGTH_SHORT
-                    ).show();
-                },
-                year,
-                month,
-                day
+                (view, selectedYear, selectedMonth, selectedDay) -> Toast.makeText(
+                        view.getContext(),
+                        String.format("%s : %s : %s", selectedDay, selectedMonth, selectedYear),
+                        Toast.LENGTH_SHORT
+                ).show(),
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
         );
-
-        return datePickerDialog;
     }
 }
