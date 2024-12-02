dfaa17c361a84daf4f419fbae792bd546239f44a
arinaldi <andrea.rinaldi2204@gmail.com>
2024-12-02 12:55:49 +0100

    test: WIP, working on adding instrumental tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,18 +36,18 @@ android {
 }
 
 dependencies {
-    implementation("androidx.fragment:fragment:1.8.1")
+    implementation("androidx.fragment:fragment:1.8.5")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.3")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.3")
-    implementation("androidx.navigation:navigation-fragment:2.7.7")
-    implementation("androidx.navigation:navigation-ui:2.7.7")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    implementation("androidx.navigation:navigation-fragment:2.8.4")
+    implementation("androidx.navigation:navigation-ui:2.8.4")
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.9.0"))
-    //implementation("com.google.dagger:hilt-android:2.45")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test:runner:1.6.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
 java {
